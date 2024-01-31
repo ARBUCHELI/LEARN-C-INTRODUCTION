cea10582a32459de453ff98fa7d3b6363e5b0d6b
ARBUCHELI <radio_seattle@hotmail.com>
2024-01-31 09:11:23 +0300

    #hello world lesson - comments

---
+++ comments/script.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  //The code outputs that the Chocolate chip cookies are better, but I totally prefer oatmeal raisin cookies
+  printf("Chocolate chip cookies are way better than oatmeal raisin cookies.\n");
+
+  /* Programming in C is maybe the most useful skill that a computer scientist can have*/
+  printf("Learning the C programming language is an exciting adventure!\n");
+}
